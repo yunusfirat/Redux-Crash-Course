ad26f0ff8286990887121097e5e10b66f202b00b
yunusfirat <yunusfirat44@gmail.com>
2021-06-09 00:59:27 +0100

    post and post form has been implemented

---
+++ src/components/Postform.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+ class Postform extends Component {
+     constructor(props) {
+         super(props)
+         this.state = {
+             title:"",
+             body:""
+         }
+         this.onChange = this.onChange.bind(this)
+     }
+onChange(e){
+    this.setState({ [e.target.name]: e.target.value })
+}
+
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <form>
+                    <div>
+                        <label>Title:</label><br />
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
+                    </div>
+                    <div>
+                        <label>Body:</label><br />
+                        <textarea name="body" onChange={this.onChange}  value={this.state.body} />
+                    </div>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default Postform;
