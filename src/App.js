ffe089106df230c130d2752ada89baa8389051a2
yunusfirat <yunusfirat44@gmail.com>
2021-06-09 02:11:15 +0100

    reducer,store and actions has been completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Posts from "./components/Posts"
 import Postform from "./components/Postform"
+import { Provider } from "react-redux";
+import store from "./store"
+
+
+//  I have installed npm i redux react-redux redux-thunk
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <Postform />
       <hr />
       <Posts />
     </div>
+    </Provider>
   );
 }
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react'
-
+import { connect } from "react-redux"
+import { fetchPosts } from "../actions/postActions"
 class Posts extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            posts: []
-        };
-    }
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(res => res.json())
-        .then(data => this.setState({ posts: data },  console.log(data)));
-    }
-
     render() {
         const postItems = this.state.posts.map(post => (
             <div key={post.id}>
@@ -30,4 +19,4 @@ class Posts extends Component {
 }
 
 
-export default Posts;
+export default connect(null, { fetchPosts })(Posts);
